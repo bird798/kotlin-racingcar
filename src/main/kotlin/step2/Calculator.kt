d0c259692f759644ad6e4ff99386cf2aa14e3006
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-13 22:17:45 +0900

    * Testcase - DisplayName추가. - 반복되는 단언문 삭제
    * Calculator
    - 열거형에 함수를 대입하여 When문을 대체
    - 구분문자(" ")를 상수로 정의
    - 불필요한 주석 제거

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,71 +1,47 @@
 package step2
 
 class Calculator() {
+    enum class OPERATOR(val operator: String, val func: (Double, Double) -> Double) {
+        PLUS("+", { a, b -> a + b }),
+        MINUS("-", { a, b -> a - b }),
+        MULTIPLY("*", { a, b -> a * b }),
+        DIVIDE("/", { a, b -> a / b }),
+        ;
+
+        companion object {
+            val map = values().associateBy(OPERATOR::operator)
+
+            fun get(operation: String): OPERATOR? {
+                return map[operation]
+            }
+        }
+    }
+
+    companion object {
+        const val SEPARATOR = " "
+    }
+
     fun calculate(formula: String): Double {
         val factors = split(formula)
-        // 리스트가 비어 있거나, Factor가 홀수가 아니거나.
         if (factors.isEmpty() || (factors.size % 2 != 1)) {
             throw IllegalArgumentException("잘못된 수식입니다.")
         }
 
-        var result = 0.0
-        var operation = ""
-        for (i in 0 until factors.size step 1) {
-            if ((i % 2) == 0) {
-                // 숫자가 나올 차례
-                val number = factors[i].toDoubleOrNull() ?: throw IllegalArgumentException("잘못된 수식입니다.")
-                when (operation) {
-                    "+" -> result = plus(result, number)
-                    "-" -> result = minus(result, number)
-                    "*" -> result = multiply(result, number)
-                    "/" -> result = divide(result, number)
-                    "" -> result = number // 저장된 연산자가 없으면 현재것 저장
-                    else -> throw IllegalArgumentException("정의되지 않은 연산자입니다.")
-                }
-                operation = ""
-            } else {
-                // 연산자가 나올 차례
-                if (factors[i] !in listOf("+", "-", "*", "/")) throw IllegalArgumentException("알수 없는 연산자입니다.")
-                operation = factors[i]
-            }
+        var result = factors[0].toDoubleOrNull() ?: throw IllegalArgumentException("잘못된 수식입니다.")
+        for (i in 1 until factors.size step 2) {
+            val operator = OPERATOR.get(factors[i]) ?: throw IllegalArgumentException("정의되지 않은 연산자입니다.")
+            val number = factors[i + 1].toDoubleOrNull() ?: throw IllegalArgumentException("잘못된 수식입니다.")
+            result = operator.func(result, number)
         }
 
         return result
     }
 
     private fun split(formula: String): List<String> {
-        val factors = formula.trim().split(" ")
+        val factors = formula.trim().split(SEPARATOR)
         if (factors.isEmpty()) {
             throw IllegalArgumentException()
         }
         return factors
-    }
-
-    private fun plus(
-        num1: Double,
-        num2: Double,
-    ): Double {
-        return num1 + num2
-    }
-
-    private fun minus(
-        num1: Double,
-        num2: Double,
-    ): Double {
-        return num1 - num2
-    }
-
-    private fun multiply(
-        num1: Double,
-        num2: Double,
-    ): Double {
-        return num1 * num2
-    }
-
-    private fun divide(
-        num1: Double,
-        num2: Double,
-    ): Double {
-        return num1 / num2
     }
 }
