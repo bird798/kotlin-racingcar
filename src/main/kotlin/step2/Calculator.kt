7ff7e703fc0d51fcc48dfeaf4663b7c544311c20
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-14 09:19:24 +0900

    1. 변수명 변경 2. Error Message 상수값으로 변경 3. 중복된 에러 처리 삭제

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -1,7 +1,7 @@
 package step2
 
 class Calculator() {
-    enum class OPERATOR(val operator: String, val func: (Double, Double) -> Double) {
+    enum class OPERATOR(val operator: String, val apply: (Double, Double) -> Double) {
         PLUS("+", { a, b -> a + b }),
         MINUS("-", { a, b -> a - b }),
         MULTIPLY("*", { a, b -> a * b }),
@@ -9,7 +9,7 @@ class Calculator() {
         ;
 
         companion object {
-            val map = values().associateBy(OPERATOR::operator)
+            private val map = entries.associateBy(OPERATOR::operator)
 
             fun get(operation: String): OPERATOR? {
                 return map[operation]
@@ -19,29 +19,30 @@ class Calculator() {
 
     companion object {
         const val SEPARATOR = " "
+
+        const val ERROR_MESSAGE_ILLEGAL_FORMULAS = "정의되지 않은 연산자입니다."
+        const val ERROR_MESSAGE_ILLEGAL_OPERATOR = "정의되지 않은 연산자입니다."
+        const val ERROR_MESSAGE_ILLEGAL_OPERAND = "숫자가 아닌값이 입력되었습니다."
     }
 
-    fun calculate(formula: String): Double {
-        val factors = split(formula)
+    fun calculate(formulas: String): Double {
+        val factors = split(formulas)
         if (factors.isEmpty() || (factors.size % 2 != 1)) {
-            throw IllegalArgumentException("잘못된 수식입니다.")
+            throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_FORMULAS)
         }
 
-        var result = factors[0].toDoubleOrNull() ?: throw IllegalArgumentException("잘못된 수식입니다.")
+        var result = factors[0].toDoubleOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERAND)
         for (i in 1 until factors.size step 2) {
-            val operator = OPERATOR.get(factors[i]) ?: throw IllegalArgumentException("정의되지 않은 연산자입니다.")
-            val number = factors[i + 1].toDoubleOrNull() ?: throw IllegalArgumentException("잘못된 수식입니다.")
-            result = operator.func(result, number)
+            val operator = OPERATOR.get(factors[i]) ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERATOR)
+            val operand = factors[i + 1].toDoubleOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_ILLEGAL_OPERAND)
+            result = operator.apply(result, operand)
         }
 
         return result
     }
 
-    private fun split(formula: String): List<String> {
-        val factors = formula.trim().split(SEPARATOR)
-        if (factors.isEmpty()) {
-            throw IllegalArgumentException()
-        }
+    private fun split(formulas: String): List<String> {
+        val factors = formulas.trim().split(SEPARATOR)
         return factors
     }
 }
