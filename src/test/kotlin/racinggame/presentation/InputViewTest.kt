5a840573e2ae8c90e57710fdaa83e408cb083cd4
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-19 10:06:14 +0900

    test(문자열 입력) : 문자열 입력 관련 테스트 추가.

--- src/test/kotlin/racinggame/presentation/InputViewTest.kt
+++ src/test/kotlin/racinggame/presentation/InputViewTest.kt
@@ -3,6 +3,7 @@ package racinggame.presentation
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class InputViewTest {
@@ -14,8 +15,29 @@ class InputViewTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["a", "", "-1", "0"])
-    fun `입력받은 문자열의 유효성체크 기능을 테스트한다`(value: String?) {
+    fun `입력받은 숫자의 유효성체크 기능을 테스트한다`(value: String?) {
         val input = InputView("Test")
-        assertThat(input.isValid(value)).isEqualTo(false)
+        assertThat(input.isValidNumber(value)).isEqualTo(false)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["   ", "abcdefg", "", "abcdefg, a"])
+    fun `입력받은 문자열을 유효성 기능을 테스트한다`(value: String) {
+        val input = InputView("Test")
+        assertThatIllegalArgumentException().isThrownBy { input.split(value, ",", 5) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "'a,b', 'a', 'b'",
+    )
+    fun `입력받은 문자열을 분해하는 기능을 테스트한다`(
+        value: String,
+        value2: String,
+        value3: String,
+    ) {
+        val input = InputView("Test")
+        val resultList = listOf(value2, value3)
+        assertThat(input.split(value, ",", 5)).isEqualTo(resultList)
     }
 }
