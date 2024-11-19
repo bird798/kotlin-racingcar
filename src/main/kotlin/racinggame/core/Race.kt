43fac8837b1e79a253ce31e9abdf75d7416b3d60
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-19 10:02:09 +0900

    feat : 레이싱 승자를 구하는 기능 추가

--- src/main/kotlin/racinggame/core/Race.kt
+++ src/main/kotlin/racinggame/core/Race.kt
@@ -5,16 +5,26 @@ import racinggame.core.condition.MoveCondition
 object Race {
     private var rounds: MutableList<Round> = mutableListOf()
 
+    fun reset() {
+        rounds = mutableListOf()
+    }
+
     fun goRound(
         cars: MutableList<Car>,
         moveCondition: MoveCondition,
     ) {
         cars.forEach { car -> car.move(moveCondition) }
-
         rounds.add(Round(cars.map { it.copy() }))
     }
 
-    fun racingResult(): List<Round> {
+    fun roundResult(): List<Round> {
         return rounds
+    }
+
+    fun racingWinner(): List<Car> {
+        require(rounds.isNotEmpty())
+        val lastRound = rounds.last()
+        val cars = lastRound.cars.sortedByDescending { it.position }
+        return cars.filter { it.position == cars.first().position }
     }
 }
