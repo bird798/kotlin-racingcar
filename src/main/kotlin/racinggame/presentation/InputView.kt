481b11e39e68052a57fe9b39caab289827f7560b
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-19 09:59:37 +0900

    feat(input) 문자열 이름 요구사항 반영
    문자열을 입력받고, ","로 Split하여 반환하는 기능 구현

--- src/main/kotlin/racinggame/presentation/InputView.kt
+++ src/main/kotlin/racinggame/presentation/InputView.kt
@@ -4,29 +4,46 @@ class InputView(val guide: String) {
     companion object {
         private const val ERROR_INVALID_STRING = "잘못 입력하셨습니다."
         private const val ERROR_GUIDE_IS_EMPTY = "가이드 문구가 없습니다"
-        private const val RETRY_MAX_COUNT = 5
     }
 
     init {
         require(guide.isNotBlank()) { ERROR_GUIDE_IS_EMPTY }
     }
 
-    fun inputNumber(): Int {
-        for (i in 0 until RETRY_MAX_COUNT) {
-            println(guide)
+    fun inputStrings(
+        delimiter: String,
+        maxLength: Int,
+    ): List<String> {
+        println(guide)
+        val value = readlnOrNull()
 
-            val value = readlnOrNull()
-            if (isValid(value)) {
-                return value!!.toInt()
-            }
-
-            println(ERROR_INVALID_STRING)
-        }
-
-        return 0
+        return split(value, delimiter, maxLength)
     }
 
-    fun isValid(value: String?): Boolean {
+    fun split(
+        value: String?,
+        delimiter: String,
+        maxLength: Int,
+    ): List<String> {
+        require(!value.isNullOrBlank()) { ERROR_INVALID_STRING }
+
+        val strings = value.trim().split(delimiter)
+        strings.forEach { str ->
+            require(str.length <= maxLength) { ERROR_INVALID_STRING }
+        }
+
+        return strings
+    }
+
+    fun inputNumber(): Int {
+        println(guide)
+
+        val value = readlnOrNull()
+        require(isValidNumber(value)) { ERROR_INVALID_STRING }
+        return value!!.toInt()
+    }
+
+    fun isValidNumber(value: String?): Boolean {
         val convertedValue = value?.toIntOrNull()
         return convertedValue != null && convertedValue > 0
     }
