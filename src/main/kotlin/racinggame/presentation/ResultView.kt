9ce0813bf9bb0d64940c71584129c7e4730f4e80
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-11-19 10:04:20 +0900

    feat : 레이싱 승자를 출력하는 기능 추가.

--- src/main/kotlin/racinggame/presentation/ResultView.kt
+++ src/main/kotlin/racinggame/presentation/ResultView.kt
@@ -1,5 +1,6 @@
 package racinggame.presentation
 
+import racinggame.core.Car
 import racinggame.core.Round
 
 class ResultView() {
@@ -16,10 +17,36 @@ class ResultView() {
         println(strBuilder.toString())
     }
 
-    fun drawRacingResult(rounds: List<Round>) {
+    fun drawWinner(cars: List<Car>) {
+        val strBuilder = StringBuilder()
+
+        cars.forEachIndexed { index, car ->
+            appendName(strBuilder, car.name, index)
+        }
+        strBuilder.append("가 최종 우승했습니다.")
+        println(strBuilder.toString())
+    }
+
+    private fun appendName(
+        strBuilder: StringBuilder,
+        value: String,
+        index: Int,
+    ) {
+        if (index > 0) {
+            strBuilder.append(", ")
+        }
+
+        strBuilder.append(value)
+    }
+
+    fun drawRacingResult(
+        rounds: List<Round>,
+        winner: List<Car>,
+    ) {
         println("실행 결과")
         rounds.forEach { round ->
             drawRoundResult(round)
         }
+        drawWinner(winner)
     }
 }
